14fc2cc18df920a310a8d736c0825fe3f08d98fa
BlessingOwuEsan <blessingowuesan@gmail.com>
2023-02-23 16:30:16 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+/* This a header file containting declarations 
+* for prototype functions used in the 0x02-functions_nested_loops
+* task and directory
+* THis is like creating my own library with declared functions
+*/
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
